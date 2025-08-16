104678173712ea578166177b08e38920a2ae3bd3
bmcabrera1 <bcabrera2195@gmail.com>
2025-08-15 23:22:59 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'examen_parcial_3',
+  webDir: 'dist'
+};
+
+export default config;
